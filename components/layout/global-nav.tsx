d18bb4dea850978ef4a1774ddcb2f1b67b7f379a
karyanayandi <halo@yandi.me>
2024-05-16 15:32:08 +0700

    add padding top on desktop global nav

--- components/layout/global-nav.tsx
+++ components/layout/global-nav.tsx
@@ -9,7 +9,7 @@ const GlobalNav: React.FC = async () => {
 
   return (
     <aside>
-      <nav className="fixed bottom-0 left-0 z-50 flex w-full border-t border-border bg-white lg:top-0 lg:block lg:w-[92px] lg:border-r">
+      <nav className="fixed bottom-0 left-0 z-50 flex w-full border-t border-border bg-white lg:top-0 lg:block lg:w-[92px] lg:border-r lg:pt-[4.5rem]">
         <NextLink
           className="group relative flex h-[92px] w-[92px] flex-1 flex-col items-center justify-center overflow-hidden text-base text-black transition-all hover:bg-muted"
           href="/"
