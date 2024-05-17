292068bda9bc3ae138be0f31de8ef780e5fb5d77
karyanayandi <halo@yandi.me>
2024-05-17 18:50:25 +0700

    add setting dashboard

---
+++ app/[locale]/(dashboard)/dashboard/setting/form.tsx
@@ -0,0 +1,136 @@
+"use client"
+
+import * as React from "react"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/toast/use-toast"
+import { useI18n, useScopedI18n } from "@/lib/locales/client"
+import { api } from "@/lib/trpc/react"
+
+interface FormValues {
+  profit_percentage: string
+  support_email: string
+}
+
+interface UpsertSettingFormProps {
+  settings: Record<string, string>
+}
+
+export default function UpsertSettingForm(props: UpsertSettingFormProps) {
+  const { settings } = props
+
+  const [loading, setLoading] = React.useState<boolean>(false)
+
+  const t = useI18n()
+  const tsSetting = useScopedI18n("setting")
+  const tsTopUp = useScopedI18n("top_up")
+
+  const { mutate: upsertSetting } = api.setting.upsert.useMutation({
+    onSuccess: () => {
+      form.reset()
+      toast({ variant: "success", description: tsSetting("upsert_success") })
+    },
+    onError: (error) => {
+      setLoading(false)
+      const errorData = error?.data?.zodError?.fieldErrors
+
+      if (errorData) {
+        for (const field in errorData) {
+          if (errorData.hasOwnProperty(field)) {
+            errorData[field]?.forEach((errorMessage) => {
+              toast({
+                variant: "danger",
+                description: errorMessage,
+              })
+            })
+          }
+        }
+      } else {
+        toast({
+          variant: "danger",
+          description: tsSetting("upsert_failed"),
+        })
+      }
+    },
+  })
+
+  const form = useForm<FormValues>({ defaultValues: settings })
+
+  const onSubmit = (values: FormValues) => {
+    setLoading(true)
+    const mergedValues = {
+      ...values,
+    }
+    const upsertValues = {
+      key: "settings",
+      value: JSON.stringify(mergedValues),
+    }
+    upsertSetting(upsertValues)
+    setLoading(false)
+  }
+
+  return (
+    <div className="mx-0 space-y-4 lg:mx-8 lg:p-5">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <h1 className="pb-2 lg:pb-5">{t("setting")}</h1>
+          <div className="flex max-w-2xl flex-col space-y-4">
+            <FormField
+              control={form.control}
+              name="profit_percentage"
+              rules={{
+                required: tsTopUp("profit_percentage_required"),
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{tsTopUp("profit_percentage")}</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder={tsTopUp("profit_percentage_required")}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="support_email"
+              rules={{
+                required: t("support_email_required"),
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("support_email")}</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder={t("support_email_placeholder")}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button aria-label="Submit" type="submit" loading={loading}>
+              {t("submit")}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
+}
