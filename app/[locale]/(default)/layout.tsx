1067112993c236b3d268cf5b5e7cb8a1645e4912
karyanayandi <xkaryanayandi@gmail.com>
2024-06-02 18:45:39 +0700

    change gradient color

--- app/[locale]/(default)/layout.tsx
+++ app/[locale]/(default)/layout.tsx
@@ -22,7 +22,7 @@ export default function DefaultLayout(props: DefaultLayoutProps) {
             aria-hidden="true"
           >
             <div
-              className="relative left-[calc(50%)] aspect-[1155/678] w-[56.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-purple-600 to-pink-400 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              className="relative left-[calc(50%)] aspect-[1155/678] w-[56.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#079992] to-[#78e08f] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
               style={{
                 clipPath:
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -34,7 +34,7 @@ export default function DefaultLayout(props: DefaultLayoutProps) {
             aria-hidden="true"
           >
             <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-pink-400 to-purple-600 opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              className="oopacity-20 relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#78e08f] to-[#079992] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
               style={{
                 clipPath:
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
