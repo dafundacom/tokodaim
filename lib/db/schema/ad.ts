21e474c3df9061cd93ad34d801ae27944201ea51
karyanayandi <xkaryanayandi@gmail.com>
2024-06-26 23:27:42 +0700

    fix ad schema

--- lib/db/schema/ad.ts
+++ lib/db/schema/ad.ts
@@ -8,10 +8,7 @@ export const ads = pgTable("ads", {
   id: text("id").primaryKey(),
   title: text("title").unique().notNull(),
   content: text("content").notNull(),
-  position: adPositionEnum("position")
-    .default("home_below_header")
-    .notNull()
-    .default("home_below_header"),
+  position: adPositionEnum("position").notNull().default("home_below_header"),
   active: boolean("active").notNull().default(true),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
