dd70352db61ca330e2fdb1fe14b9fa513dedd705
karyanayandi <xkaryanayandi@gmail.com>
2024-09-17 20:09:47 +0700

    add new product button on dashboard

--- app/[locale]/(dashboard)/dashboard/product/header.tsx
+++ app/[locale]/(dashboard)/dashboard/product/header.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 
+import DashboardAddNew from "@/components/dashboard/dashboard-add-new"
 import DashboardHeading from "@/components/dashboard/dashboard-heading"
 import { useI18n } from "@/lib/locales/client"
 
@@ -11,6 +12,7 @@ export default function DashboardProductHeader() {
   return (
     <div className="mb-8 flex justify-between">
       <DashboardHeading>{t("product")}</DashboardHeading>
+      <DashboardAddNew url="/dashboard/product/new" />
     </div>
   )
 }
