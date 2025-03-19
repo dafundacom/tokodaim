6666d5d3b53cdcfd0dad6e7b9c979e92355784f3
Karyana Yandi <karyana@yandi.me>
2025-03-19 22:34:43 +0700

    feat: add shared UI component library
    - Create new @tokodaim/ui package with Button & Badge components
    - Add styling utilities with TailwindCSS themes and variables
    - Configure tailwind configs in dashboard and web apps
    - Set up proper package dependencies and exports

---
+++ packages/ui/README.md
@@ -0,0 +1,4 @@
+# @tokodaim/ui
+
+This project is licensed under the terms of the
+[MIT license](https://github.com/dafundacom/tokodaim/blob/main/LICENSE.md).
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,16 @@
+import baseConfig from "@yopem/eslint-config/base"
+import reactConfig from "@yopem/eslint-config/react"
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ["dist/**"],
+  },
+  ...baseConfig,
+  ...reactConfig,
+  {
+    rules: {
+      "no-restricted-imports": "off",
+    },
+  },
+]
